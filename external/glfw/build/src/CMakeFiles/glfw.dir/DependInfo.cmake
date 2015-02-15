bb3572e0e64d3a59bcd3267134bcf3b3765a1efb
Kevin Thielen <kevathiel@gmail.com>
2015-02-15 11:31:55 +0100

    added external libraries

---
+++ external/glfw/build/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/src/clipboard.c" "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/build/src/CMakeFiles/glfw.dir/clipboard.c.o"
+  "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/src/context.c" "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/build/src/CMakeFiles/glfw.dir/context.c.o"
+  "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/src/gamma.c" "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/build/src/CMakeFiles/glfw.dir/gamma.c.o"
+  "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/src/glx_context.c" "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/build/src/CMakeFiles/glfw.dir/glx_context.c.o"
+  "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/src/init.c" "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/build/src/CMakeFiles/glfw.dir/init.c.o"
+  "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/src/input.c" "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/build/src/CMakeFiles/glfw.dir/input.c.o"
+  "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/src/joystick.c" "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/build/src/CMakeFiles/glfw.dir/joystick.c.o"
+  "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/src/monitor.c" "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/build/src/CMakeFiles/glfw.dir/monitor.c.o"
+  "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/src/time.c" "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/build/src/CMakeFiles/glfw.dir/time.c.o"
+  "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/src/window.c" "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/build/src/CMakeFiles/glfw.dir/window.c.o"
+  "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/src/x11_clipboard.c" "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/build/src/CMakeFiles/glfw.dir/x11_clipboard.c.o"
+  "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/src/x11_gamma.c" "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/build/src/CMakeFiles/glfw.dir/x11_gamma.c.o"
+  "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/src/x11_init.c" "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/build/src/CMakeFiles/glfw.dir/x11_init.c.o"
+  "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/src/x11_joystick.c" "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/build/src/CMakeFiles/glfw.dir/x11_joystick.c.o"
+  "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/src/x11_monitor.c" "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/build/src/CMakeFiles/glfw.dir/x11_monitor.c.o"
+  "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/src/x11_time.c" "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/build/src/CMakeFiles/glfw.dir/x11_time.c.o"
+  "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/src/x11_unicode.c" "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/build/src/CMakeFiles/glfw.dir/x11_unicode.c.o"
+  "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/src/x11_window.c" "/home/kevin/Desktop/Projects/C++/Engine/external/glfw/build/src/CMakeFiles/glfw.dir/x11_window.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "_GLFW_USE_CONFIG_H"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  "src"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
